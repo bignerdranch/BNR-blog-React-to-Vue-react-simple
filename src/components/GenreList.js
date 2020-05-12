d62af3aa0487c855d647fd8561ce304a928087c2
Julian Kyer <julian.kyer@gmail.com>
2020-05-12 16:04:08 -0600

    use hooks in genrelist

--- src/components/GenreList.js
+++ src/components/GenreList.js
@@ -1,29 +1,27 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import GenreCard from "./GenreCard";
 import "./GenreList.css";
 import genreService from "../services/genreService";
 
-class GenreList extends Component {
-  state = {
-    genres: [],
-  };
+const GenreList = () => {
+  const [genres, setGenres] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     genreService
       .getGenres()
-      .then((res) => this.setState({ genres: res.data }))
+      .then((response) => {
+        setGenres(response.data);
+      })
       .catch((error) => console.log("There was an error", error));
-  }
+  });
 
-  render() {
-    return (
-      <div className="genre-list">
-        {this.state.genres.map((genre) => (
-          <GenreCard genre={genre} key={genre.id} />
-        ))}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="genre-list">
+      {genres.map((genre) => (
+        <GenreCard genre={genre} key={genre.id} />
+      ))}
+    </div>
+  );
+};
 
 export default GenreList;
